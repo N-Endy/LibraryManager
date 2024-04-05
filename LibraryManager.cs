0988c7479fe4f994e2378409f8015c05339d706c
nelson@wiretooth.com <nelson@wiretooth.com>
2024-04-05 20:15:27 +0100

    Update LibraryManager

--- LibraryManager.cs
+++ LibraryManager.cs
@@ -6,43 +6,47 @@ public class LibraryManager
     private readonly IBookRepository _bookRepository;
 
 
-    public LibraryManager(ILibraryService libraryService, ILibraryUserInteraction libraryUserInteraction, IMemberRepository memberRepository)
+    public LibraryManager(ILibraryService libraryService, ILibraryUserInteraction libraryUserInteraction, IMemberRepository memberRepository, IBookRepository bookRepository)
     {
         _libraryService = libraryService;
         _libraryUserInteraction = libraryUserInteraction;
         _memberRepository = memberRepository;
+        _bookRepository = bookRepository;
     }
 
     public void Run()
     {
-        _libraryUserInteraction.PromptToSelectOption();
-        var userChoice = Console.ReadLine();
-
-        switch (userChoice)
+        while (true)
         {
-            case "1":
-                Member member = _libraryUserInteraction.CreateMember();
-                _memberRepository.AddMember(member);
-                break;
-            case "2":
-                _libraryUserInteraction.ShowMessage("\nEnter Member ID to remove.");
-                var memberId = Console.ReadLine();
-                _memberRepository.DeleteMember(memberId);
-                break;
-            case "3":
-                Member updateMember = _libraryUserInteraction.CreateMember();
-                _memberRepository.UpdateMember(updateMember);
-                break;
-            case "4":
-                Book updateBook = _libraryUserInteraction.CreateBook();
-                _bookRepository.UpdateBook(updateBook);
-                break;
-            case "6":
-                _libraryUserInteraction.Exit();
-                break;
-            default:
-                _libraryUserInteraction.ShowMessage("Invalid choice. Please choose again.");
-                break;
+            _libraryUserInteraction.PromptToSelectOption();
+            var userChoice = Console.ReadLine();
+
+            switch (userChoice)
+            {
+                case "1":
+                    Member member = _libraryUserInteraction.CreateMember();
+                    _memberRepository.AddMember(member);
+                    break;
+                case "2":
+                    _libraryUserInteraction.ShowMessage("\nEnter Member ID to remove.");
+                    var memberId = Console.ReadLine();
+                    _memberRepository.DeleteMember(memberId);
+                    break;
+                case "3":
+                    Member updateMember = _libraryUserInteraction.CreateMember();
+                    _memberRepository.UpdateMember(updateMember);
+                    break;
+                case "4":
+                    Book updateBook = _libraryUserInteraction.CreateBook();
+                    _bookRepository.UpdateBook(updateBook);
+                    break;
+                case "6":
+                    _libraryUserInteraction.Exit();
+                    break;
+                default:
+                    _libraryUserInteraction.ShowMessage("Invalid choice. Please choose again.");
+                    break;
+            }
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,8 @@
-﻿ILibraryService libraryService = new LibraryService();
+﻿IBookRepository bookRepository = new BookRepository();
+IMemberRepository memberRepository = new MemberRepository();
+ILibraryUserInteraction libraryUserInteraction = new LibraryUserInteraction();
+ILibraryService libraryService = new LibraryService(memberRepository, bookRepository);
 
-var libraryManager = new LibraryManager();
+var libraryManager = new LibraryManager(libraryService, libraryUserInteraction, memberRepository, bookRepository);
+
+libraryManager.Run();
